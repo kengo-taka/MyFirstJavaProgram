155055b37c3e00db5d990bd55461da9e536520b5
kengo-taka <1117marine1117@gmail.com>
2020-10-05 11:13:52 -0700

    first commit

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,22 @@
+public class MyFirstJavaProgram {
+
+  public static void main(String[] args) {
+    byte numByte = 127;
+    short numShort = 32000;
+    int numInt = 2000000000;
+    long numLong = 9000000;
+    float numFloat = 12.222f;
+    double numDouble = 506.111111;
+    boolean isTrue = true;
+    char strChar= 'b';
+  
+    System.out.println(numByte);
+    System.out.println(numShort);
+    System.out.println(numInt);
+    System.out.println(numLong);
+    System.out.println(numFloat);
+    System.out.println(numDouble);
+    System.out.println(isTrue);
+    System.out.println(strChar);
+  }
+}
